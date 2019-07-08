70bbf3d3bea40477c624fdb107912c5a39a4a56c
davidemvf <davidemv.fiore@gmail.com>
2019-07-08 18:31:08 +0200

    completati

--- dis-pari/play.js
+++ dis-pari/play.js
@@ -1,26 +1,30 @@
 // L’utente sceglie pari o dispari e un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer e dichiariamo chi ha vinto.
 
+
+// Creo variabili per la scelta pari o dispari da parte dell'utente, definisco le due variabili con un valore random da 1 a 5
 var utente = prompt("Pari o dispari?")
 var n_uno = parseInt(1 + Math.random()*5);
 var n_due = parseInt(1 + Math.random()*5);
+
+// richiamo le due funzioni tramite variabili: la prima somma i numeri, la seconda verifica la parità della somma;
 var somma = somma(n_uno, n_due);
 var parita = parita(somma);
 
-
+// visualizzo tramite console i numeri usciti, la loro somma e la parità di quest'ultima;
 console.log("Primo numero: ", n_uno);
 console.log("Secondo numero: ", n_due);
 console.log("Somma: ", somma);
 console.log("Pari o dispari?", parita);
 
+// definisco le condizioni di vittoria e sconfitta e visualizzo il risultato;
 if (utente === parita) {
   document.writeln("Hai vinto!");
 } else {
   document.writeln("Hai perso!");
 }
 
-
-
+// funzioni utilizzate:
 function somma( n_uno, n_due ){
   var risultato = n_uno + n_due;
   return risultato;
